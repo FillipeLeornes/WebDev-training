9b735342a21a16bc96ecd8e7b7a64d7329539a8a
Biggie <fillipe.leornes95@gmail.com>
2023-02-18 23:51:10 -0300

    estrutura-basica-landing-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.app}>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
       </header>
+      <main className={styles.main}>
+        <section>
+          <h1>Welcome to My Landing Page</h1>
+          <p>This is a regular landing page built with React.js.</p>
+          <button>Get Started</button>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; My Landing Page 2023</p>
+      </footer>
     </div>
   );
 }
 
+App.propTypes = {
+  className: PropTypes.string,
+};
+
 export default App;
+
